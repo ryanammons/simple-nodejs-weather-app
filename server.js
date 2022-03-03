07334dea1e5ea495ca45881b4fd313b3fbd90b1f
Ryan Ammons <ryan@ammonsdatasolutions.com>
2022-03-03 17:54:47 -0500

    Update server.js

--- server.js
+++ server.js
@@ -32,6 +32,6 @@ app.post('/', function (req, res) {
   });
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(80, function () {
+  console.log('Example app listening on port 80!')
 })
